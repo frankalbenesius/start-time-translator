f15c42995c9ae8e9831501a85fd7f720161eeddf
frankalbenesius <frankalbenesius@gmail.com>
2020-05-30 12:34:04 -0500

    Add translation code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [time, setTime] = useState("");
+
+  function handleTimeChange(e) {
+    e.preventDefault();
+    setTime(e.target.value);
+  }
+
+  const [hour, minute] = time.split(":");
+  const actualTime = [parseInt(hour) + 1, parseInt(minute) + 45].join(":");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Start Time Translator</h1>
+      <div className="timeBlock">
+        <label htmlFor="appt">Time Your Friends Say</label>
+        <input value={time} onChange={handleTimeChange} type="time" id="appt" />
+      </div>
+      <div className="timeBlock">
+        <label htmlFor="appt2">Time They Actually Mean</label>
+        <input value={actualTime} disabled type="time" id="appt2" />
+      </div>
     </div>
   );
 }

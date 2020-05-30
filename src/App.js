91d325a8bca811a18097715cebe8cb126552b8df
frankalbenesius <frankalbenesius@gmail.com>
2020-05-30 12:46:08 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -9,8 +9,16 @@ function App() {
     setTime(e.target.value);
   }
 
-  const [hour, minute] = time.split(":");
-  const actualTime = [parseInt(hour) + 1, parseInt(minute) + 45].join(":");
+  const [hours, minutes] = time.split(":");
+  let actual = "";
+  if (hours && minutes) {
+    const h = parseInt(hours);
+    const m = parseInt(minutes);
+    const isPm = h >= 12;
+    const fixedHours = h > 12 ? h - 12 : h;
+    const actualHours = fixedHours + 2;
+    actual = `${actualHours}:${m} ${isPm ? "PM" : "AM"}`;
+  }
 
   return (
     <div className="App">
@@ -21,7 +29,7 @@ function App() {
       </div>
       <div className="timeBlock">
         <label htmlFor="appt2">Time They Actually Mean</label>
-        <input value={actualTime} disabled type="time" id="appt2" />
+        <div className="actual">{actual}</div>
       </div>
     </div>
   );
